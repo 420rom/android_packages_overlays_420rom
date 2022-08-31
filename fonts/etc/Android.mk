a790c1737a430e70daff9edf3a26a93d5702a7f1
abun880007 <abun880007@msn.com>
2022-08-31 01:55:34 +0100

    420rom time

--- fonts/etc/Android.mk
+++ fonts/etc/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := fonts_customization.xml
-LOCAL_SRC_FILES := fonts_customization.xml
+LOCAL_MODULE := fonts_customisation.xml
+LOCAL_SRC_FILES := fonts_customisation.xml
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_PRODUCT_MODULE := true
